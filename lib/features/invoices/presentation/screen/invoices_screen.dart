f6c2c1380f0ec2384bfddbd2fc5666310587c858
Ahmedkawba <ahmedkawbal7@gmail.com>
2025-06-20 22:55:48 +0300

    Add OCR functionality with custom floating action button
    - Implemented CustomFloatingactionButton widget to allow users to select or capture images for OCR processing.
    - Integrated Tesseract OCR for text extraction from images, supporting Arabic and English languages.
    - Added configuration for trained data files in JSON format and ensured necessary files are copied to the app's document directory.
    - Enhanced user experience by managing loading states during image selection and OCR processing.

--- lib/features/invoices/presentation/screen/invoices_screen.dart
+++ lib/features/invoices/presentation/screen/invoices_screen.dart
@@ -1,11 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_quill/flutter_quill.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:invoice_ocr_app/core/app/routes.dart';
+
+import 'widgets/custom_floatingaction_button.dart';
 
 class InvoicesScreen extends StatefulWidget {
   const InvoicesScreen({super.key});
@@ -16,74 +11,22 @@ class InvoicesScreen extends StatefulWidget {
 
 class _InvoicesScreenState extends State<InvoicesScreen> {
   bool isLoading = false;
-  File? imageFile;
 
-  String scannedText = "";
+  void updateLoading(bool value) {
+    setState(() {
+      isLoading = value;
+    });
+  }
 
-  final ImagePicker picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: isLoading
           ? Center(child: CircularProgressIndicator())
           : Center(child: Text("Invoices Screen")),
-      floatingActionButton: SpeedDial(
-        icon: Icons.add,
-        activeIcon: Icons.close,
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        spacing: 10,
-        spaceBetweenChildren: 8,
-        children: [
-          SpeedDialChild(
-            child: Icon(Icons.image),
-            label: 'اختر صورة',
-            onTap: () => getImage(ImageSource.gallery),
-          ),
-          SpeedDialChild(
-            child: Icon(Icons.camera_alt),
-            label: 'التقاط',
-            onTap: () => getImage(ImageSource.camera),
-          ),
-        ],
+      floatingActionButton: CustomFloatingactionButton(
+        onLoadingChanged: updateLoading,
       ),
-    );
-  }
-
-  void getImage(ImageSource source) async {
-    try {
-      final pickedImage = await ImagePicker().pickImage(source: source);
-      if (pickedImage != null) {
-        imageFile = File(pickedImage.path);
-        isLoading = true;
-        setState(() {});
-        getRecognisedText(File(pickedImage.path));
-      }
-    } catch (e) {
-      imageFile = null;
-      isLoading = false;
-      scannedText = "Error occured while scanning";
-      setState(() {});
-    }
-  }
-
-  void getRecognisedText(File image) async {
-    final inputImage = InputImage.fromFilePath(image.path);
-    final textRecognizer = TextRecognizer();
-    RecognizedText recognisedText = await textRecognizer.processImage(
-      inputImage,
-    );
-    String extractedText = recognisedText.text;
-
-    final doc = Document()..insert(0, extractedText);
-
-    isLoading = false;
-    setState(() {});
-
-    Navigator.pushNamed(
-      context,
-      Routes.referenceInvoice,
-      arguments: doc.toDelta().toJson(),
     );
   }
 }
---
+++ lib/features/invoices/presentation/screen/widgets/custom_floatingaction_button.dart
@@ -0,0 +1,125 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+import 'package:tesseract_ocr/ocr_engine_config.dart';
+import 'package:tesseract_ocr/tesseract_ocr.dart';
+
+import '../../../../../core/app/routes.dart';
+
+class CustomFloatingactionButton extends StatefulWidget {
+  const CustomFloatingactionButton({super.key, required this.onLoadingChanged});
+  final Function(bool) onLoadingChanged; // دالة لتعديل الحالة في الوالد
+
+  @override
+  State<CustomFloatingactionButton> createState() =>
+      _CustomFloatingactionButtonState();
+}
+
+class _CustomFloatingactionButtonState
+    extends State<CustomFloatingactionButton> {
+  File? imageFile;
+
+  String scannedText = "";
+
+  final ImagePicker picker = ImagePicker();
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return SpeedDial(
+      icon: Icons.add,
+      activeIcon: Icons.close,
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      spacing: 10,
+      spaceBetweenChildren: 8,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.image),
+          label: 'اختر صورة',
+          onTap: () => _getImage(ImageSource.gallery),
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.camera_alt),
+          label: 'التقاط',
+          onTap: () => _getImage(ImageSource.camera),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _getImage(ImageSource source) async {
+    try {
+      final pickedImage = await picker.pickImage(source: source);
+      if (pickedImage == null) return;
+
+      setState(() {
+        widget.onLoadingChanged(true); // تأكد من إعادة الحالة إلى false
+        imageFile = File(pickedImage.path);
+      });
+
+      await _performOcr(imageFile!.path);
+    } catch (e) {
+      setState(() {
+        widget.onLoadingChanged(false); // تأكد من إعادة الحالة إلى false
+      });
+      debugPrint('خطأ أثناء اختيار الصورة: $e');
+    }
+  }
+
+  Future<void> _performOcr(String imagePath) async {
+    try {
+      await _copyTrainedDataIfNeeded();
+
+      final tesseractConfig = OCRConfig(
+        language: 'ara+eng',
+        engine: OCREngine.tesseract,
+        options: {
+          TesseractConfig.preserveInterwordSpaces: '1',
+          TesseractConfig.pageSegMode: PageSegmentationMode.auto,
+        },
+      );
+
+      final extractedText = await TesseractOcr.extractText(
+        imagePath,
+        config: tesseractConfig,
+      );
+
+      final doc = Document()..insert(0, extractedText);
+
+      widget.onLoadingChanged(false); // تأكد من إعادة الحالة إلى false
+
+      Navigator.pushNamed(
+        context,
+        Routes.referenceInvoice,
+        arguments: doc.toDelta().toJson(),
+      );
+    } catch (e) {
+      widget.onLoadingChanged(true); // تأكد من إعادة الحالة إلى false
+      debugPrint('Error performing OCR: $e');
+    }
+  }
+
+  Future<void> _copyTrainedDataIfNeeded() async {
+    final appDir = await getApplicationDocumentsDirectory();
+    final tessdataDir = Directory(path.join(appDir.path, 'tessdata'));
+
+    if (!await tessdataDir.exists()) {
+      await tessdataDir.create(recursive: true);
+    }
+
+    for (final lang in ['ara', 'eng']) {
+      final file = File('${tessdataDir.path}/$lang.traineddata');
+      if (!await file.exists()) {
+        final byteData = await DefaultAssetBundle.of(
+          context,
+        ).load('assets/tessdata/$lang.traineddata');
+        await file.writeAsBytes(byteData.buffer.asUint8List());
+      }
+    }
+  }
+}
